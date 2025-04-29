121687641121abc8ae60830cbf48513ac2ad14a1
Vesta Ballah <135280117+TCorvah@users.noreply.github.com>
2025-04-29 17:01:26 -0500

    making changes to system interface

--- Vehicle_Project_PhaseOne/inc/system_interface.ads
+++ Vehicle_Project_PhaseOne/inc/system_interface.ads
@@ -7,11 +7,24 @@ with Standard_Vehicle; use Standard_Vehicle;
 with Vehicle_Constants; use Vehicle_Constants;
 with Ada.Float_Text_IO; use Ada.Float_Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Vehicle_Types; use Vehicle_Types;
 
 package System_Interface is
 
+   -- Declare the vehicle type
+   -- Luxury vehicle type
+   Selected_Vehicle : Vehicle_Types.Vehicle_Type;
+
+
    -- Procedure to run the system interface
-   procedure Run_System_Interface( Vehicles : in out Luxury_Vehicle.Luxury_Car);
+   procedure Run_System_Interface( Selected_Type : in out Vehicle_Types.Vehicle_Type);
+   -- Procedure to run the luxury vehicle interface
+   procedure Run_Luxury_Scenario( Vehicles : in out Luxury_Vehicle.Luxury_Car);
+   -- Procedure to run the standard vehicle interface    
+   procedure Run_Standard_Scenario( Vehicles : in out Standard_Vehicle.Standard);
+
+   -- Procedure to activate the time of day
+   procedure Activate_TOD(Time : in out Sensor_System.Sensor);
 
 end System_Interface;
 
---
+++ Vehicle_Project_PhaseOne/inc/vehicle_types.ads
@@ -0,0 +1,5 @@
+package Vehicle_Types is
+
+   type Vehicle_Type is (Luxury, Standard_cars);
+
+end Vehicle_Types;
