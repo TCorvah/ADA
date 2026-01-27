f513a6b5e656c6c55d35159da27d1eacb7308622
Vesta Ballah <135280117+TCorvah@users.noreply.github.com>
2026-01-26 21:04:25 -0600

    Add radar detection with sector-based angular and range classification

--- Vehicle_PhaseTwo/src/radar_systems.adb
+++ Vehicle_PhaseTwo/src/radar_systems.adb
@@ -3,7 +3,6 @@ with Ada.Float_Text_IO; use Ada.Float_Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Radar_Systems; use Radar_Systems;
 with Ada.Numerics.Float_Random; use Ada.Numerics.Float_Random;
-with Road_ProfileConfig; use Road_ProfileConfig;
 with Vehicle_Constants; use Vehicle_Constants;
 
 
@@ -31,15 +30,50 @@ package body Radar_Systems is
       Put_Line ("Radar: Deactivated");
    end Deactivate_Radar;
 
-   function Is_Clear_To_Move(Radar_Data : in Radar; Threshold : Float) return Boolean is
+   --- Function to get the center angle of a radar sector based on its quadrants.
+   --- The sector center angle is returned  return the midpoint angle of 
+   --  the specified radar sector in each quadrant.
+   function Sector_Center_Angle(Sector : Radar_Sector) return Float is
    begin
-      return Radar_Data.Object_Distance > Threshold;
-   end Is_Clear_To_Move;
+      case Sector is
+         when Front =>
+            return  Vehicle_Constants.Front_Sector_Midpoint; -- 0 degrees midpoint angle for Front sector (315 to 45 degrees)
+         when Rear =>
+            return Vehicle_Constants.Rear_Sector_Midpoint; -- 180 degrees midpoint angle for Rear sector(135 to 225 degrees)
+         when Left =>
+            return Vehicle_Constants.Left_Sector_Midpoint; -- 270 degrees midpoint  angle for Left sector (225 to 315 degrees)
+         when Right =>
+            return Vehicle_Constants.Right_Sector_Midpoint; -- 90 degrees midpoint angle for Right sector( 45 to 135 degrees)
+      end case;
+   end Sector_Center_Angle;
+   
 
+   --- Function to get the radar sector based on the given angle for each quadrant.
+   -- The function normalizes the angle to ensure it falls within the range of 0 to 360 degrees.
+   -- EaCch sector represents a 90-degree quadrant of the 360-degree field of view.
+   function Get_Sector_Angle(Sector : Float) return Radar_Sector is
+      A : Float := Normalize_Angle(Sector);
+   begin
+      if (A >= Vehicle_Constants.Left_Sector_End) or else  (A < Vehicle_Constants.Front_Sector_End) then
+         return Radar_Systems.Front;
+      elsif A >= Vehicle_Constants.Right_Sector_Start and then A < Vehicle_Constants.Right_Sector_End then
+         return Radar_Systems.Right;
+      elsif A >= Vehicle_Constants.Rear_Sector_Start and then A < Vehicle_Constants.Rear_Sector_End then
+         return Radar_Systems.Rear;
+      elsif A >= Vehicle_Constants.Left_Sector_Start and then A < Vehicle_Constants.Left_Sector_End   then
+         return Radar_Systems.Left;
+      else
+         return Radar_Systems.Front; -- Should never reach here if normalization works
+      end if;
+   end Get_Sector_Angle;
+
+
+
+ -- Math helper to normalize the angle to be in compliance of the geometric circle.
    function Normalize_Angle(Angle : Float) return Float is
       Norm : Float := Angle;
    begin
-      while Norm < 0.0 loop
+      while Norm < Vehicle_Constants.Front_Sector_Midpoint loop
          Norm := Norm + Vehicle_Constants.Full_Circle_Angle;
       end loop;
       while Norm >= Vehicle_Constants.Full_Circle_Angle loop
@@ -48,204 +82,108 @@ package body Radar_Systems is
       return Norm;
    end Normalize_Angle;
 
-   function Get_Sector_Angle(Sector : Float) return Radar_Sector is
-      A : Float := Normalize_Angle(Sector);
-   begin
-      if (A >= 315.0 or A < 45.0) then
-         return Radar_Systems.Front;
-      elsif A >= 45.0 and A < 135.0 then
-         return Radar_Systems.Right;
-      elsif A >= 135.0 and A < 225.0 then
-         return Radar_Systems.Rear;
-      elsif A >= 225.0 and A < 315.0 then
-         return Radar_Systems.Left;
-      else
-         return Radar_Systems.Front; -- Should never reach here if normalization works
-      end if;
-   end Get_Sector_Angle;
-
-     
-
-   function Sector_Center_Angle(Sector : Radar_Sector) return Float is
-   begin
-      case Sector is
-         when Front =>
-            return 0.0; -- Front sector angle range
-         when Rear =>
-            return 180.0; -- Rear sector angle range
-         when Left =>
-            return 270.0; -- Left sector angle range
-         when Right =>
-            return 90.0; -- Right sector angle range
-      end case;
-   end Sector_Center_Angle;
-
-   function Analyze_Radar_Data(Distance: Float; Road_Profile : Road_ProfileConfig.Road_Profile) return Radar_Data is
-   begin
-      if Distance <= Road_Profile.Min_Detection_Range then
-         return Emergency_Stop; -- Emergency stop if object is too close
-      elsif Distance <= Road_Profile.Caution_Distance then
-         return Slow_Down; -- Slow down if object is detected within caution distance
-      elsif Distance <= Road_Profile.Max_Detection_Range then
-         return Caution; -- Caution if object is detected within caution distance
-      else
-         return Clear_To_Move; -- Clear to move if no objects are detected
-      end if;
-   end Analyze_Radar_Data;
-
-
-   procedure Radar_Scan_Highway_Simulation is
-      Scan_Count : Float := 0.0;
-      Max_Scans : constant Float := 5.0;
-      Gen : Generator;
-      Object_Distance : Float;
-
-      Angle : Float;
-       --simulate the radar scan for random distance and angle
-      function Random_Angle return Float is
+   --simulate the radar scan for random distance between min and max detection range
+   -- A random number generator is used to generate random distances within the radar's detection range.
+   -- A radar can only detect objects within its specified minimum and maximum detection range.
+   Gen : Generator;
+   function Random_Distance return Float is
+      Rand : Float;
       begin
-         return -45.0 + (Random(Gen) * 90.0); -- Random distance between 0 and 100 meters
-      end Random_Angle;
-
-      --simulate the radar scan for random distance and angle
-      function Random_Distance return Float is
-      begin
-         return Random(Gen) * 100.0; -- Random distance between 0 and 100 meters
+         Rand := Vehicle_Constants.Minimum_Detection_Range + (Random(Gen) * (Vehicle_Constants.Maximum_Detection_Range - Vehicle_Constants.Minimum_Detection_Range));
+         return Rand;
       end Random_Distance;
-   begin
-      Reset (Gen);
-      Put_Line ("Radar: Scanning...");
-      loop
-         exit when Scan_Count = Max_Scans;
-         Angle := Random_Angle; -- Scan across Line of Sight
-         Object_Distance := Random_Distance;
-         Put("Angle: ");
-         Put(Angle);
-         Put("Degree,  Distance to object: ");
-         Put(Object_Distance);
-         Put_Line ("meters");
-         if Object_Distance <=  Vehicle_Constants.Minimum_Detection_Range then
-            Put_Line ("Radar: Emergency brake! Object too close");
-         elsif Object_Distance <= Vehicle_Constants.Min_Caution_Distance then
-            Put_Line ("Radar: Slow down! Object detected at distance: " & Float'Image(Object_Distance) & " meters, angle: " & Float'Image(Angle));
-         elsif Object_Distance > Vehicle_Constants.Min_Caution_Distance and Object_Distance <= Vehicle_Constants.Maximum_Detection_Range then
-            Put_Line ("Radar: Caution! Object detected at distance: " & Float'Image (Object_Distance) & " meters, angle: " & Float'Image(Angle));
-         else
-            Put_Line ("Radar: >> Path Clear: ");
-         end if;
-         delay 2.5; -- Simulate time delay for radar scan
-         Scan_Count := Scan_Count + 1.0;
-         if Scan_Count = Max_Scans then
-            Put_Line ("Radar: Scan completed.");
-         end if;
-      end loop;
-      Put_Line ("Radar: Scan completed.");
-   end Radar_Scan_Highway_Simulation;
 
-   procedure Radar_Scan_Garage_Simulation(Road : Road_ProfileConfig.Road_Profile) is
-      Scan_Count : Integer := 0;
-      Max_Scans : constant Integer := 5;
-      Gen : Generator;
-      Distance : Float;
-      Angle : Float;
+   -- Simulates a radar detection angle within a single radar sector.
+   -- The generated angle is uniformly distributed within the sector's
+   -- field of view, defined as ±Detection_Angle degrees from the
+   -- sector's center angle.
+   -- This ensures detections remain fully contained within the sector
+   -- and do not overlap adjacent sectors.
+   function Random_Angle return Float is
+      Center_Angle : Radar;
+   begin
+      return Center_Angle.Center_Angle - Vehicle_Constants.Detection_Angle  + Random(Gen) * (2.0 * Vehicle_Constants.Detection_Angle);     
+   end Random_Angle;
+
+   -- the function is to determine the angular accuracy of the detected object within the radar sector.
+   -- A high accuracy of detection is when the object angle is within one-third of the detection angle from the sector midpoint.    
+   function Angular_Accuracy_Zone(Angle_Diff: Float) return Radar_Angular_Zone is
+   begin 
+      if Angle_Diff <= Vehicle_Constants.Detection_Angle / 3.0 then
+         return Strong;
+      elsif Angle_Diff <= (2.0 * Vehicle_Constants.Detection_Angle) / 3.0 then
+         return Medium;
+      elsif Angle_Diff <= Vehicle_Constants.Detection_Angle then
+         return Weak;
+      else
+         return Outside_FOV;
+      end if;
+   end Angular_Accuracy_Zone;
+
+
+   function Range_Accuracy_Zone(Distance : Float) return Radar_Range_Zone is
+   begin
+       if Distance < (Vehicle_Constants.Minimum_Detection_Range) then
+         return Outside_Range;
+      elsif Distance <= (Vehicle_Constants.Maximum_Detection_Range / 4.0) then   
+         return Close;
+      elsif Distance <= Vehicle_Constants.Maximum_Detection_Range then
+         return Far;
+      else
+         return Outside_Range;
+      end if;
+   end Range_Accuracy_Zone;
+
+   -- Function to simulate radar object detection based on object distance and angle.
+   -- The function determines the radar sector, calculates the angle difference from the sector center,
+   function Detect_Object(Object_Distance : Float; Object_Angle : Float) return Radar is
       Sector : Radar_Sector;
-     
-       --simulate random angle from -45 to 315 degrees(full scan)
-      function Random_Angle return Float is
-      begin
-         return -45.0 + (Random(Gen) * 360.0); -- Random angle between -45 and 315 degrees
-      end Random_Angle;
-
-      --simulate the radar scan for random distance
-      function Random_Distance return Float is
-      begin
-         return Random(Gen) * Road.Max_Detection_Range; -- Random distance between 0 and 40 meters
-      end Random_Distance;
+      Center_Angle : Float;
+      Angle_Diff : Float;
+      Angular_Zone : Radar_Angular_Zone;
+      Distance_Zone : Radar_Range_Zone;
+      Data : Radar;
    begin
-      Reset (Gen);
-      Put_Line ("Radar: starting Garage scan...");
-      while Scan_Count < Max_Scans loop
-         Angle := Random_Angle; -- Generate a random angle for the scan
-         Distance := Random_Distance; -- Generate a random distance for the scan
-         Sector := Get_Sector_Angle(Angle); -- Determine the sector based on the angle
-         Put_Line("");
-         Put_Line("Scan #" & Integer'Image(Scan_Count + 1) & ":");
+      -- Determine the radar sector based on the object angle
+      Sector := Get_Sector_Angle(Object_Angle);
 
-         Put("  Angle    : ");
-         Float_IO.Put(Angle, Fore => 6, Aft => 1, Exp => 0);
-         Put_Line("°");
+      -- Get the center angle of the determined sector
+      Center_Angle := Sector_Center_Angle(Sector);
 
-         Put("  Distance : ");
-         Float_IO.Put(Distance, Fore => 6, Aft => 1, Exp => 0);
-         Put_Line(" m");
+      -- Calculate the absolute difference between the object angle and the sector center angle
+      Angle_Diff  := abs(Normalize_Angle(Object_Angle) - Center_angle);
+      if Angle_Diff > (Vehicle_Constants.Angle_Sector_FOV * 2.0) then
+         Angle_Diff := Vehicle_Constants.Full_Circle_Angle - Angle_Diff;
+      end if;
 
-         Put("  Sector   : " & Radar_Sector'Image(Sector) & 
-               " (center: ");
-         Float_IO.Put(Sector_Center_Angle(Sector), Fore => 4, Aft => 1, Exp => 0);
-         Put_Line("°)");
+      -- Determine the detection zone based on the angle difference
+      Angular_Zone  := Angular_Accuracy_Zone(Angle_Diff);
 
+      if Angular_Zone  = Outside_FOV then
+         Data.Object_Detected := False;
+         return Data;
+      end if;
 
-         case Sector is
-            when Radar_Systems.Front =>
-               Put_Line ("Front Sector Scan for objects:");
-            when Radar_Systems.Rear =>
-               Put_Line ("Rear Sector Scan for objects: ");
-            when Radar_Systems.Left =>
-               Put_Line ("Left Sector Scan for objects: ");
-            when Radar_Systems.Right =>
-               Put_Line ("Right Sector Scan for objects: ");
-         end case;
-         --Distance checks
-         declare
-            Status : Radar_Data := Analyze_Radar_Data(Distance, Road);
-         begin
-            case Status is
-               when Emergency_Stop =>
-                  Put("Radar: Emergency brake! Object detected too close: ");
-                  Float_IO.Put(Distance, Fore => 6, Aft => 1, Exp => 0);
-                  Put(" m, angle: ");
-                  Float_IO.Put(Angle, Fore => 6, Aft => 1, Exp => 0);
-                  Put_Line("°");
+      -- Determine the distance zone based on the object distance
+      Distance_Zone := Range_Accuracy_Zone(Object_Distance);
+      if Distance_Zone = Outside_Range then
+         Data.Object_Detected := False;
+         return Data;
+      end if;
 
-               when Slow_Down =>
-                  Put("Radar: Slow down! Object detected at distance: ");
-                  Float_IO.Put(Distance, Fore => 6, Aft => 1, Exp => 0);
-                  Put(" m, angle: ");
-                  Float_IO.Put(Angle, Fore => 6, Aft => 1, Exp => 0);
-                  Put_Line("°");
+      -- fill detection record
+      Data.Object_Detected := True;
+      Data.Object_Distance := Object_Distance;
+      Data.Object_Angle := Object_Angle;     
+      Data.Center_Angle := Center_angle;
+      Data.Angular_Zone := Angular_Zone;
+      Data.Sector := Sector;
+      return Data;
+   end Detect_Object;
+        
 
-               when Caution =>
-                  Put("Radar: Caution! Object detected at distance: ");
-                  Float_IO.Put(Distance, Fore => 6, Aft => 1, Exp => 0);
-                  Put(" m, angle: ");
-                  Float_IO.Put(Angle, Fore => 6, Aft => 1, Exp => 0);
-                  Put_Line("°");
-
-               when Clear_To_Move =>
-                  Put_Line("Radar: >> Path Clear: ");
-                  Put_Line("No objects detected in the " & Radar_Sector'Image(Sector) & " sector.");
-                  Put_Line("Vehicle can move freely");
-            end case;
-            if Status /= Clear_To_Move then
-               Float_IO.Put(Distance, Fore => 6, Aft => 1, Exp => 0);
-               Put(" m, angle: ");
-               Float_IO.Put(Angle, Fore => 6, Aft => 1, Exp => 0);
-               Put_Line("°");
-               --Put_Line("Radar: >> Path Blocked: ");
-               --Put_Line("Object detected in the " & Radar_Sector'Image(Sector) & " sector.");
-            end if;
-         end;
-            
-         delay 2.5; -- Simulate time delay for radar scan
-         Scan_Count := Scan_Count + 1; 
-         if Scan_Count = Max_Scans then
-            Put_Line ("Radar: Garage scan completed.");
-         end if;
-      
-      end loop;
-   end Radar_Scan_Garage_Simulation;
-      
-   
+  
+  
 
 
 end Radar_Systems;   
